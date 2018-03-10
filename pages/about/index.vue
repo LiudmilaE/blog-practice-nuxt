886b8b1e162af9011593005ddc8cf8fc8e596351
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2018-03-10 23:52:33 +0300

    Initial commit

---
+++ pages/about/index.vue
@@ -0,0 +1,23 @@
+<template>
+  	<div class="about-page">
+      	<h1>This is my fantastic "About Me" Page!</h1>
+		<p>Lorem ipsum</p>
+		<p>Lorem ipsum</p>
+		<p>Lorem ipsum</p>
+  	</div>
+</template>
+
+<script>
+    export default {
+    
+    }
+</script>
+
+<style scoped>
+	.about-page {
+		text-align: center;
+		padding: 30px;
+	}
+</style>
+
+
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="home=page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview 
+        id="1" 
+        title="Lorem ipsum1" 
+        previewText="Text - lorem ipsum" 
+        thumbnail="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb"/>
+      <PostPreview 
+        id="2" 
+        title="Lorem ipsum2" 
+        previewText="Text - lorem ipsum" 
+        thumbnail="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb"/>
+      <PostPreview 
+        id="3" 
+        title="Lorem ipsum3" 
+        previewText="Text - lorem ipsum" 
+        thumbnail="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+  .intro {
+    height: 300px;
+    position: relative;
+    padding: 30px;
+    box-sizing: border-box;
+    background-image: url('~assets/main-image.jpeg');
+    /* background-position: center; */
+    background-size: cover;
+  }
+
+  .intro h1 {
+    position: absolute;
+    top: 10%;
+    left: 5%;
+    width: 90%;
+    font-size: 1.5rem;
+    color: black;
+    background-color: rgb(211, 211, 211);
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 3px 3px 3px black;
+    box-sizing: border-box;
+    border: 1px solid black;
+  }
+
+  @media (min-width: 768px) {
+    .intro h1 {
+      font-size: 2rem;
+    }
+  }
+
+  .featured-posts {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
