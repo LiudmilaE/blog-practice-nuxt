68c6b390b7556e6188a34eab2b86ddb7887b4d37
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2018-03-25 22:16:07 +0300

    Add promise instead of callback

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -7,29 +7,38 @@
 <script>
 import PostList from "@/components/Posts/PostList";
 export default {
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          { 
-            id: '1', 
-            title: "A very important post", 
-            previewText: 'Lorem ipsum...', 
-            thumbnail: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
-          },
-          { 
-            id: '2', 
-            title: "This post will change a lot for you :)", 
-            previewText: 'Lorem ipsum...', 
-            thumbnail: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
-          },
-        ]
-      })
-    }, 500);
+  components: {
+    PostList
   },
-	components: {
-		PostList
-	}
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            { 
+              id: '1', 
+              title: "A very important post", 
+              previewText: 'Lorem ipsum...', 
+              thumbnail: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
+            },
+            { 
+              id: '2', 
+              title: "This post will change a lot for you :)", 
+              previewText: 'Lorem ipsum...', 
+              thumbnail: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
+            },
+          ]
+        });
+    }, 500);
+    })
+    .then(data => data)
+    .catch(e => {
+      context.error(e);
+    });
+  },
+  created() {
+    this.$store.dispatch('setPosts', this.loadedPosts)
+  }
 }
 </script>
 
