1b225c55d95dd27386c7bf849f7359216bcf2de2
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2018-03-25 22:57:20 +0300

    Use data from store and nuxtServerInit

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -10,37 +10,6 @@ export default {
   components: {
     PostList
   },
-  fetch(context) {
-    // if (context.store.state.loadedPosts.length > 0) {
-    //   return null;
-    // }
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            { 
-              id: '1', 
-              title: "A very important post", 
-              previewText: 'Lorem ipsum...', 
-              thumbnail: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
-            },
-            { 
-              id: '2', 
-              title: "This post will change a lot for you :)", 
-              previewText: 'Lorem ipsum...', 
-              thumbnail: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
-            },
-          ]
-        });
-    }, 500);
-    })
-    .then(data => {
-      context.store.comit('setPosts', data.loadedPosts)
-    })
-    .catch(e => {
-      context.error(e);
-    });
-  },
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts
--- store/index.js
+++ store/index.js
@@ -11,6 +11,34 @@ const createStore = () => {
             }
         },
         actions: {
+            nuxtServerInit(vueContext, context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                      vuexContext.commit('setPosts', [
+                          { 
+                            id: '1', 
+                            title: "A very important post", 
+                            previewText: 'Lorem ipsum...', 
+                            thumbnail: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
+                          },
+                          { 
+                            id: '2', 
+                            title: "This post will change a lot for you :)", 
+                            previewText: 'Lorem ipsum...', 
+                            thumbnail: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb'
+                          },
+                        ]
+                      );
+                      resolve();
+                  }, 500);
+                  })
+                  .then(data => {
+                    context.store.comit('setPosts', data.loadedPosts)
+                  })
+                  .catch(e => {
+                    context.error(e);
+                  });
+            },
             setPosts(vuexContext, posts) {
                 vuexContext.commit('setPosts', posts)
             }
